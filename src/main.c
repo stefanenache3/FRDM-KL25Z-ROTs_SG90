77d84064c8342277e4522ece290c9a3aa350ae7b
luca.mitran <luca.mitran@mta.ro>
2023-12-30 16:34:17 +0200

    Add Uart module ->Vector for RX

--- src/main.c
+++ src/main.c
@@ -1,10 +1,14 @@
 #include "MKL25Z4.h"
 #include "Sequencer.h"
 #include "pit.h"
+#include "uart.h"
 int main(void){
 	
+	UART0_Initialize(115200);
 	led_init();
 	PIT_Init();
-	while(1){}
+	while(1){
+		
+	}
 	return 0;
 }
